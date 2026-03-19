7520f299a95f8cb240d585caaed9ec6540a996f4
Jonathan Schuster <jonathan.schuster080501@gmail.com>
2026-03-19 22:07:28 +0100

    fix: Cleanup-Service-Worker löscht alte Caches automatisch

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+// Service Worker – löscht alle alten Caches und deregistriert sich selbst
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', async () => {
+  const keys = await caches.keys()
+  await Promise.all(keys.map(k => caches.delete(k)))
+  await self.registration.unregister()
+})
